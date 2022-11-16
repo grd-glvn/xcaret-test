19888cf56ae7b02879f8c25482ec41b86df18391
grd-glvn <The-Northman@github.com>
2022-11-16 12:43:53 -0600

    Legals, prefooter and footer responsive

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,11 @@ import { useSelector } from 'react-redux'
 function Footer() {
     const content = useSelector( state => state.ui.content );
   return (
-    <footer className="px-[10%] flex flex-row flex-wrap justify-between items-center bg-black pt-5 pb-5">
+    <footer className="sm:px-[10%] flex flex-row flex-wrap sm:justify-between items-center justify-center bg-black pt-5 pb-5">
     <p className="text-white text-xs" >{content.footer.copy}</p>
-    <div className="">
-      <a className="mx-10 text-white text-xs" href={content.footer.links[0].href}>{content.footer.links[0].tittle}</a>
-      <a className="text-white text-xs" href={content.footer.links[1].href}>{content.footer.links[1].tittle}</a>
+    <div className="flex gap-32 sm:gap-0 my-5">
+      <a className="sm:mx-10 text-white text-xs order-2 sm:order-1" href={content.footer.links[0].href}>{content.footer.links[0].tittle}</a>
+      <a className="text-white text-xs order-1 sm:order-2" href={content.footer.links[1].href}>{content.footer.links[1].tittle}</a>
     </div>
   </footer>
   )
--- components/Legals.js
+++ components/Legals.js
@@ -4,8 +4,8 @@ function Legals() {
     const content = useSelector( state => state.ui.content );
     
   return (
-    <div  className="mt-16 bg-gray-200 grid grid-cols-5">
-    <p className='pl-28 py-10 col-span-3 text-sm' dangerouslySetInnerHTML={{__html:content.legals}}></p>
+    <div  className="sm:mt-16 bg-gray-200 sm:grid sm:grid-cols-5">
+    <p className='px-3 py-5 sm:pl-28 sm:py-10 sm:col-span-3 text-sm' dangerouslySetInnerHTML={{__html:content.legals}}></p>
     </div>
   )
 }
